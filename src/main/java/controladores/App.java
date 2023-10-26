4ce7975a6baca24087fc9027c9e6f39ac1914a8f
DESKTOP-31VBQ8V\Puesto18 <raulcorderodiaz@gmail.com>
2023-10-26 14:29:37 +0200

    no message

---
+++ src/main/java/controladores/App.java
@@ -0,0 +1,22 @@
+package controladores;
+
+import daos.Acceso;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Acceso acceso = new Acceso("Usu","Acceso usuarios biblioteca");
+		Acceso acceso2 = new Acceso("Emp","Empleados biblioteca");
+		em.persist(acceso);
+		em.persist(acceso2);
+		em.getTransaction().commit();
+		em.close();
+    }
+}
---
+++ src/main/java/daos/Acceso.java
@@ -0,0 +1,42 @@
+package daos;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Accesos", schema = "Biblioteca")
+public class Acceso {
+	// ATRIBUTOS
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_acceso", nullable = false)
+	private long id_acceso;
+
+	@Column(name = "codigo_acceso", nullable = false)
+	private String codigo_acceso;
+
+	@Column(name = "descripcion_acceso")
+	private String descripcion_acceso;
+
+	@OneToMany(mappedBy = "acceso")
+	List<Usuario> usuariosConAcceso;
+
+	// CONSTRUCTORES
+	public Acceso() {
+		super();
+	}
+
+	public Acceso(String codigo_acceso, String descripcion_acceso) {
+		super();
+
+		this.codigo_acceso = codigo_acceso;
+		this.descripcion_acceso = descripcion_acceso;
+	}
+}
